d2988f9f49559139dc1400bf538bf3b5d38e6fe8
ashutoshrai4755 <ashutoshrai274302@gmail.com>
2026-09-13 23:53:56 +0000

    add datatypes

--- js_basics/datatypes.js
+++ js_basics/datatypes.js
@@ -1,3 +1,12 @@
 "use strict"; // treat all JS code as newer version
+// "use strict" ka matlab hai — JS engine ko bolna: "mujhe strict/careful mode mein chalao, jahan galtiyan jo pehle silently ignore hoti thi, ab error ban jaayengi."
+//"Newer version" ka matlab hota — naye features milna (jaise let, const, arrow functions, classes waghera — ye sab ES6+ ki cheezein hain).
 
-alert(3+3)
+//alert(3+3) // we are using nodejs, not browser. It means alert is a feature which is given by browser and node js is not a browser. its a runtime environment.
+console.log(3+3); console.log("Ashu") //readability is not there
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Ashu")
