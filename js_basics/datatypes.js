2d2503407e8e2bb5d3a0b7892d3a6f3628cdef84
ashutoshrai4755 <ashutoshrai274302@gmail.com>
2026-09-17 18:24:09 +0000

    datatypes

--- js_basics/datatypes.js
+++ js_basics/datatypes.js
@@ -10,3 +10,28 @@ console.log(3
     3) // code readability should be high
 
 console.log("Ashu")
+
+
+//DATA TYPES
+
+let name = "Ashu"
+let age = 21
+let isLoggedIn = true
+let state
+
+//PRIMITIVE DATATYPES=> immutable
+
+//number => range is 2 to power of 53
+//bigint => to store values beyond number. oftenly we don't use it. it used in mainly trading related, in big websites etc;
+//string => used ""/''
+//boolean => true/false
+//null => standalone value
+//undefined => just declared
+//symbol => unique
+
+
+//NON PRIMITIVE DATATYPES => mutable
+//object
+
+console.log(typeof null); //undefined
+console.log(typeof undefined); //object
